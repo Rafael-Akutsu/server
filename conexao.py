f15b4939694a420dc8cc81b2c6d04e19b3bb8073
Henrique Verissimo <henriquedevsp@gmail.com>
2025-07-21 13:16:03 -0300

    feat(dependencies): isolate secrets and credentials
    -add new .env file to

--- conexao.py
+++ conexao.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
 from flask_mysqldb import MySQL
 
+from os import getenv
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'senha123'
-app.config['MYSQL_DB'] = 'flask_db'
+app.config['MYSQL_HOST'] = getenv("MYSQL_HOST")
+app.config['MYSQL_USER'] = getenv("MYSQL_USER")
+app.config['MYSQL_PASSWORD'] = getenv("MYSQL_PASSWORD")
+app.config['MYSQL_DB'] = getenv("MYSQL_DB")
 
 mysql = MySQL(app)
 
